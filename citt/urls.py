5a49a4c6b47998909942a0371efe735e7ba66866
NachoCatalan <i.catalanvidal@gmail.com>
2025-05-14 13:27:24 -0400

    fixed user form

--- citt/urls.py
+++ citt/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,9 +9,3 @@ urlpatterns = [
     path('docente/', include('docente.urls')),
     ]
 
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
